e5464f28d635e4fc75529a4c0416388f4ca4e9f0
Ayrton Souza <ayrtonsouza@icloud.com>
2022-07-23 23:07:35 -0300

    [:tada:] initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
+var (
+	clientId     = "myclient"
+	clientSecret = "mKOg9NawQK8KBkJTlZcr89qbdWsLin4E"
+)
+
+func main() {
+	ctx := context.Background()
+
+	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/realms/myrealm")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := oauth2.Config{
+		ClientID:     clientId,
+		ClientSecret: clientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  "http://localhost:8081/auth/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
+	}
+
+	state := "1234567890"
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		http.Redirect(writer, request, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
